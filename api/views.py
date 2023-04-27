2444aab17102dc61a64583d537db0f33286d9363
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-27 06:37:40 -0700

    fixed bugs

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Projects, ProjectSlideshow, ProjectFunctionality, Stack
 from .serializers import ProjectSerializer, SlideShowSerializer, FunctionalitySerializer, StackSerializer
 from rest_framework.decorators import api_view
+from rest_framework import status
 # Create your views here.
 
 # @api_view(['GET'])
@@ -26,11 +27,16 @@ from rest_framework.decorators import api_view
 #     return Response(routes)
 @api_view(['GET'])
 def getProjectStack(request, pk):
-    
-    project = Projects.objects.get(id=pk)
-    stacks =  project.stack.all()
-    serializer = StackSerializer(stacks, many=True)
-    return Response(serializer.data)
+    try:
+        project = Projects.objects.get(id=pk)
+        stacks =  project.stack.all()
+        serializer = StackSerializer(stacks, many=True)
+        return Response(serializer.data)
+    except Projects.DoesNotExist:
+        return Response('Project does not exist', status=status.HTTP_404_NOT_FOUND)
+
+
+
     
 
 @api_view(['GET'])
@@ -45,21 +51,27 @@ def getProject(request, pk):
         project = Projects.objects.get(id=pk)
         serializer = ProjectSerializer(project, many=False)
         return Response(serializer.data)
-    except:
-        pass
-    return Response('Data does not exist')
+    except Projects.DoesNotExist:
+        return Response('Project does not exist', status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET'])
 def getSlideShow(request, pk):
-    image = ProjectSlideshow.objects.filter(project_id=pk)
-    serializer = SlideShowSerializer(image, many=True)
-    return Response(serializer.data)
+    try:
+        image = ProjectSlideshow.objects.filter(project_id=pk)
+        serializer = SlideShowSerializer(image, many=True)
+        return Response(serializer.data)
+    except ProjectSlideshow.DoesNotExist:
+        return Response('Project does not exist', status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(['GET'])
 def getFunctionality(request, pk):
-    functionalities = ProjectFunctionality.objects.filter(project_id=pk)
-    serializer = FunctionalitySerializer(functionalities, many=True)
-    return Response(serializer.data)
+    try:
+        functionalities = ProjectFunctionality.objects.filter(project_id=pk)
+        serializer = FunctionalitySerializer(functionalities, many=True)
+        return Response(serializer.data)
+    except ProjectFunctionality.DoesNotExist:
+        return Response('Project does not exist', status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['GET'])
@@ -67,4 +79,6 @@ def getStacks(request):
     stacks = Stack.objects.all()
     serializer = StackSerializer(stacks, many=True)
     return Response(serializer.data)
-    
+
+
+
