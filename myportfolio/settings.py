1e992ab48d7a6471464016015675ef5106af1f5b
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-27 19:15:12 -0700

    added static files dir

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -160,6 +160,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
