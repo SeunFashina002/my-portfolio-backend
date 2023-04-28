cd958f6e23c9c32c6124fd312746ffa738938ff2
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-27 20:28:55 -0700

    more on serving static files

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -168,6 +168,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/images')
+]
 
 
 MEDIA_URL = '/images/'
